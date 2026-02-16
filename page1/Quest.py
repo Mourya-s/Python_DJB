2fdf37295888832f0ee004049b89010c40353ea1
Mourya s <somu72sher@gmail.com>
2026-02-16 10:00:20 +0530

    first commit

---
+++ page1/Quest.py
@@ -0,0 +1,21 @@
+# #leap year
+# year=int(input("enter the year "))
+# if year%4 ==0:
+#     print("is a leap year")
+# else :
+#     print(f"Not a laep year")
+
+
+# #Pattern program
+# alp=97
+# for i in range(1,5):
+#     for j in range(1,5):
+#         if i==j:
+#             print(chr(alp),end=" ")
+#             alp+=1
+#         else :
+#             print(alp, end=" ")
+   
+#     print()
+
+
